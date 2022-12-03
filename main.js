996c58447d808adcec3927f9f9b60ac6220f1a8b
FTBoojux <78246614+FTBoojux@users.noreply.github.com>
2022-12-03 07:13:44 +0000

    log add

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ bot.on('message.group',(context)=>{
         let key = input.substring(0,pos)
         let word = input.substring(pos+1)
         let cqCode = word.substring(4,9)
+        console.log(`cqCode:${cqCode}`);
         if(cqCode === 'image'){
             let start = word.indexOf('url')
             word = word.substring(start+4)
