b420541439618f548b0b010a327c6a76500897b7
FTBoojux <78246614+FTBoojux@users.noreply.github.com>
2022-12-03 07:15:03 +0000

    ca word index change

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ bot.on('message.group',(context)=>{
         let pos = input.indexOf(' ')
         let key = input.substring(0,pos)
         let word = input.substring(pos+1)
-        let cqCode = word.substring(4,9)
+        let cqCode = word.substring(6,11)
         console.log(`cqCode:${cqCode}`);
         if(cqCode === 'image'){
             let start = word.indexOf('url')
